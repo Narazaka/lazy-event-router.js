33a3735fbc3287d7b7e01c981c9603c660fa1147
Narazaka <info@narazaka.net>
2016-11-05 23:35:05 +0900

    to TypeScript

--- karma.conf.js
+++ karma.conf.js
@@ -1,98 +1,58 @@
-import webpack_config from './webpack.config.js';
-import path from 'path';
+const webpackConfig = require("webpack-config-narazaka-ts-js").webTest;
 
 module.exports = (config) =>
   config.set({
-    basePath: '',
-    frameworks: ['detectBrowsers', 'mocha'],
-    plugins: [
-      'karma-mocha',
-      'karma-webpack',
-      'karma-mocha-own-reporter',
-      'karma-ie-launcher',
-      'karma-firefox-launcher',
-      'karma-chrome-launcher',
-      'karma-safari-launcher',
-      'karma-opera-launcher',
-      'karma-phantomjs-launcher',
-      'karma-detect-browsers',
-      'karma-coverage',
+    frameworks: ["detectBrowsers", "mocha"],
+    plugins:    [
+      "karma-mocha",
+      "karma-webpack",
+      "karma-mocha-own-reporter",
+      "karma-ie-launcher",
+      "karma-firefox-launcher",
+      "karma-chrome-launcher",
+      "karma-safari-launcher",
+      "karma-opera-launcher",
+      "karma-phantomjs-launcher",
+      "karma-detect-browsers",
+      "karma-coverage",
     ],
     files: [
-//      'mock/browser-global-vars.js',
-      require.resolve('mocha-lazy-bdd/dist/mocha-lazy-bdd'),
-      'mock/mocha-lazy-bdd.js',
-//      'node_modules/power-assert/build/power-assert.js',
-//      'src/**/*.js',
-      'test/**/*.js',
+      require.resolve("mocha-lazy-bdd/dist/mocha-lazy-bdd"),
+      "mock/mocha-lazy-bdd.js",
+      "test/**/*.js",
+      "test/**/*.ts",
     ],
-    exclude: ['**/*.swp'],
+    exclude:       ["**/*.swp"],
     preprocessors: {
-//      'src/**/*.js': ['webpack', 'coverage'],
-      'test/**/*.js': ['webpack'],
+      "test/**/*.js": ["webpack"],
+      "test/**/*.ts": ["webpack"],
     },
-    webpack: {
-      devtool: 'inline-source-map',
-      module: {
-        loaders: webpack_config.module.loaders,
-        postLoaders: [
-          {
-            test: /\.js$/,
-            include: path.resolve('./src/'),
-            loader: 'istanbul-instrumenter'
-          },
-        ],
-      },
-    },
-    babelPreprocessor: {
-      options: {
-        presets: ['es2015'],
-        plugins: ['babel-plugin-espower'],
-        sourceMap: true,
-      },
-      filename: (file) => file.originalPath.replace(/\.js$/, '.es5.js'),
-      sourceFileName: (file) => file.originalPath,
-    },
+    webpack:           webpackConfig,
     coverageReporter: {
-      reporters: [{type: 'lcov'}],
-      instrumenters: {isparta: require('isparta')},
-      instrumenter: {
-        '**/*.js': 'isparta',
-      },
-      instrumenterOptions: {
-        isparta: {
-          babel: {
-            presets: ['power-assert'],
-            sourceMap: true,
-          },
-        },
-      },
+      reporters:           [
+        {type: "lcov"},
+        {type: "html"},
+        {type: "text"},
+      ],
     },
-    reporters: ['mocha-own', 'coverage'],
-    detectBrowsers: {
-      postDetection: function(availableBrowsers) {
+    reporters:             ["mocha-own", "coverage"],
+    detectBrowsers:        {
+      postDetection(availableBrowsers) {
         const result = availableBrowsers;
         if (process.env.TRAVIS) {
-          const chrome_index = availableBrowsers.indexOf('Chrome');
-          if (chrome_index >= 0) {
-            result.splice(chrome_index, 1);
-            result.push('Chrome_travis_ci');
+          const chromeIndex = availableBrowsers.indexOf("Chrome");
+          if (chromeIndex >= 0) {
+            result.splice(chromeIndex, 1);
+            result.push("ChromeTravisCI");
           }
         }
         return result;
       },
     },
     customLaunchers: {
-      Chrome_travis_ci: {
-         base: 'Chrome',
-         flags: ['--no-sandbox'],
-       },
+      ChromeTravisCI: {
+        base:  "Chrome",
+        flags: ["--no-sandbox"],
+      },
     },
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: [],
-    singleRun: false,
-    concurrency: Infinity,
   });
